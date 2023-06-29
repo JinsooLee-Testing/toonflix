b9a04096f7e29d6de2650ece60e275ab3201319c
Jinsoo.lee <tmf2676@gmail.com>
2023-06-30 00:37:10 +0900

    Firt, make header column

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class Player {
   String? name;
 
@@ -9,27 +8,54 @@ class Player {
 }
 
 void main() {
-  var picco = Player( name : "picco");
+  var picco = Player(name: "picco");
   runApp(App());
-
 }
 
-
-
-class App extends StatelessWidget{
-  
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home : Scaffold(
-        appBar : AppBar(
-          title: Text('Hello Flutter!')
+      home: Scaffold(
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: Center(
-          child : Text('Hello World!')),
       ),
     );
   }
-
 }
