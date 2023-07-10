2a912a2d71259cf23b6bcc8d1b6a3b744960f971
Jinsoo.lee <tmf2676@gmail.com>
2023-07-10 23:52:44 +0900

    recv API

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/Screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 // import 'package:toonflix/widgets/button.dart';
 // import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
